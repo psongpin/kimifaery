39baeb9577b215e4f5c2e439d6c2dc0859fced10
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2021-01-21 02:01:36 +0800

    style: setup tailwind and styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app'
 
+import '../styles/globals.css'
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
