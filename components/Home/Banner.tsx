c16aa5350500b2d63f720c91feff54c2509f9772
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2021-01-27 03:29:04 +0800

    style: full screen banner

--- components/Home/Banner.tsx
+++ components/Home/Banner.tsx
@@ -11,20 +11,20 @@ const Frame = styled.div``
 
 const Banner: React.FC = () => {
   return (
-    <Frame className="bg-pink-dark relative">
-      <div className="container pt-32 md:pt-40 pb-24 flex flex-col items-center justify-center">
+    <Frame className="bg-pink-dark relative min-h-screen pb-40 md:pb-48">
+      <div className="container pt-24 md:pt-40 flex flex-col items-center justify-center">
         <Clock />
         <Notification />
 
         <motion.div
           animate={{ y: [0, 10, 20, 10, 0] }}
           transition={{ loop: Infinity, duration: 1.5, ease: 'linear' }}
-          className="container flex flex-col items-center justify-center mt-24 md:mt-40 lg:mt-64"
+          className="absolute bottom-8 flex flex-col items-center justify-center"
         >
-          <div className="w-9 h-16 border-4 border-white rounded-full mb-5 flex justify-center">
+          <div className="w-9 h-16 border-4 border-white rounded-full mb-5 flex justify-center transform scale-75 md:scale-100">
             <div className="h-4 border-2 border-white w-0 rounded-full mt-2" />
           </div>
-          <p className="text-white text-xl">{bannerContents.scrollText}</p>
+          <p className="text-white md:text-xl">{bannerContents.scrollText}</p>
         </motion.div>
       </div>
     </Frame>
