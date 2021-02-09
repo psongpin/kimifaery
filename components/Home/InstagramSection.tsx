93cbec559859a9ba3174bdc47992023dd6a73d7d
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2021-02-10 05:36:32 +0800

    feat: instagram section

--- components/Home/InstagramSection.tsx
+++ components/Home/InstagramSection.tsx
@@ -1,18 +1,40 @@
 import Button from 'components/Button'
 import { instagramSectionContents } from 'constants/home'
 
-const InstagramSection: React.FC = () => {
+interface Props {
+  instagramMediaData: {
+    alt: string
+    postUrl: string
+    src: string
+  }[]
+}
+
+const InstagramSection: React.FC<Props> = ({ instagramMediaData }) => {
   return (
     <section className="pb-32 relative">
       <div className="container">
-        <div className="flex justify-between items-center mb-12">
-          <h2 className="text-pink-darker text-4xl md:text-5xl font-bold">
+        <div className="flex justify-between flex-col md:flex-row items-center mb-12 text-center md:text-left">
+          <h2 className="text-pink-darker text-4xl md:text-5xl font-bold mb-8 md:mb-0">
             {instagramSectionContents.mainHeading}
           </h2>
 
-          <Button variant="secondary">
-            {instagramSectionContents.buttonText}
-          </Button>
+          <a
+            href="https://www.instagram.com/kimifaery/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button variant="secondary">
+              {instagramSectionContents.buttonText}
+            </Button>
+          </a>
+        </div>
+
+        <div className="grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-8 mx-auto w-80 md:w-full max-w-full">
+          {instagramMediaData.map(data => (
+            <a href={data.postUrl} target="_blank" rel="noreferrer">
+              <img key={data.postUrl} src={data.src} alt={data.alt} />
+            </a>
+          ))}
         </div>
       </div>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,37 @@ import Banner from 'components/Home/Banner'
 import InstagramSection from 'components/Home/InstagramSection'
 import IntroductionSection from 'components/Home/IntroductionSection'
 import ShowcaseSection from 'components/Home/ShowcaseSection'
+import { GetStaticProps } from 'next'
 
-const Home: React.FC = () => {
+import { getJSON, mapMedia } from 'lib/instagram'
+
+interface Props {
+  instagramMediaData: {
+    alt: string
+    postUrl: string
+    src: string
+  }[]
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://www.instagram.com/kimifaery/')
+  const body = await res.text()
+  const json = await getJSON(body)
+  const instagramMediaData = mapMedia(json)
+
+  return {
+    props: { instagramMediaData },
+  }
+}
+
+const Home: React.FC<Props> = ({ instagramMediaData }) => {
   return (
     <div className="flex-1">
       <Banner />
       <div className="bg-white relative overflow-hidden scroll-target">
         <IntroductionSection />
         <ShowcaseSection />
-        <InstagramSection />
+        <InstagramSection instagramMediaData={instagramMediaData} />
       </div>
     </div>
   )
