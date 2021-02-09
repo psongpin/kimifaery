faef709c330c2060e52f87dabb30c3d34d0d67c5
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2021-02-10 06:00:25 +0800

    fix: csr ig data

--- components/Home/InstagramSection.tsx
+++ components/Home/InstagramSection.tsx
@@ -1,15 +1,33 @@
+import { useState } from 'react'
+import { useEffectOnce } from 'react-use'
+
 import Button from 'components/Button'
 import { instagramSectionContents } from 'constants/home'
+import { getJSON, mapMedia } from 'lib/instagram'
+import Loader from 'components/Loader'
 
-interface Props {
-  instagramMediaData: {
-    alt: string
-    postUrl: string
-    src: string
-  }[]
-}
+type IGData = {
+  alt: string
+  postUrl: string
+  src: string
+}[]
 
-const InstagramSection: React.FC<Props> = ({ instagramMediaData }) => {
+const InstagramSection: React.FC = () => {
+  const [loading, setLoading] = useState(false)
+  const [instagramMediaData, setData] = useState<IGData>([])
+
+  useEffectOnce(() => {
+    const getIgData = async () => {
+      setLoading(true)
+      const res = await fetch('https://www.instagram.com/kimifaery/')
+      const body = await res.text()
+      const json = await getJSON(body)
+      setData(mapMedia(json))
+      setLoading(false)
+    }
+
+    getIgData()
+  })
   return (
     <section className="pb-32 relative">
       <div className="container">
@@ -29,13 +47,21 @@ const InstagramSection: React.FC<Props> = ({ instagramMediaData }) => {
           </a>
         </div>
 
-        <div className="grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-8 mx-auto w-80 md:w-full max-w-full">
-          {instagramMediaData.map(data => (
-            <a href={data.postUrl} target="_blank" rel="noreferrer">
-              <img key={data.postUrl} src={data.src} alt={data.alt} />
-            </a>
-          ))}
-        </div>
+        {loading && (
+          <div className="flex justify-center py-8">
+            <Loader />
+          </div>
+        )}
+
+        {instagramMediaData.length > 0 && (
+          <div className="grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-8 mx-auto w-80 md:w-full max-w-full">
+            {instagramMediaData.map(data => (
+              <a href={data.postUrl} target="_blank" rel="noreferrer">
+                <img key={data.postUrl} src={data.src} alt={data.alt} />
+              </a>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,37 +2,15 @@ import Banner from 'components/Home/Banner'
 import InstagramSection from 'components/Home/InstagramSection'
 import IntroductionSection from 'components/Home/IntroductionSection'
 import ShowcaseSection from 'components/Home/ShowcaseSection'
-import { GetStaticProps } from 'next'
 
-import { getJSON, mapMedia } from 'lib/instagram'
-
-interface Props {
-  instagramMediaData: {
-    alt: string
-    postUrl: string
-    src: string
-  }[]
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch('https://www.instagram.com/kimifaery/')
-  const body = await res.text()
-  const json = await getJSON(body)
-  const instagramMediaData = await mapMedia(json)
-
-  return {
-    props: { instagramMediaData },
-  }
-}
-
-const Home: React.FC<Props> = ({ instagramMediaData }) => {
+const Home: React.FC = () => {
   return (
     <div className="flex-1">
       <Banner />
       <div className="bg-white relative overflow-hidden scroll-target">
         <IntroductionSection />
         <ShowcaseSection />
-        <InstagramSection instagramMediaData={instagramMediaData} />
+        <InstagramSection />
       </div>
     </div>
   )
